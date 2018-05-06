0d8512b6249c051b8cd902b81d5d591ff9af9bfc
coffeehoock <coffeehoock@gmail.com>
2018-05-06 18:20:45 +0300

    "18-05-06 18:20:45"

--- assets/coffee/react.jsx
+++ assets/coffee/react.jsx
@@ -5,136 +5,40 @@ import createClass from 'create-react-class'
 import {BrowserRouter as Router ,Route ,Link ,browserHistory} from 'react-router-dom'
 
 
-// For this demo, we are using the UMD build of react-router-dom
-const {
-    HashRouter,
-    Switch,
-    Route,
-    Link
-} = ReactRouterDOM
+// console.log(BrowserRouter);
+// import 'react-router'
 
-// A simple data API that will be used to get the data for our
-// components. On a real website, a more robust data fetching
-// solution would be more appropriate.
-const PlayerAPI = {
-    players: [
-        { number: 1, name: "Ben Blocker", position: "G" },
-        { number: 2, name: "Dave Defender", position: "D" },
-        { number: 3, name: "Sam Sweeper", position: "D" },
-        { number: 4, name: "Matt Midfielder", position: "M" },
-        { number: 5, name: "William Winger", position: "M" },
-        { number: 6, name: "Fillipe Forward", position: "F" }
-    ],
-    all: function() { return this.players},
-    get: function(id) {
-        const isPlayer = p => p.number === id
-        return this.players.find(isPlayer)
-    }
-}
+// import {BrowserRouter ,Route ,Link} from 'react-router-dom'
+// // import createBrowerHistory from 'history/CreateBrowerHistory'
+//
+// const history = createBrowerHistory();
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+// require('babel-preset-react');
+// require('jsx');
 
-// The FullRoster iterates over all of the players and creates
-// a link to their profile page.
-const FullRoster = () => (
-    <div>
-        <ul>
-            {
-                PlayerAPI.all().map(p => (
-                    <li key={p.number}>
-                        <Link to={`/roster/${p.number}`}>{p.name}</Link>
-                    </li>
-                ))
-            }
-        </ul>
-    </div>
-)
 
-// The Player looks up the player using the number parsed from
-// the URL's pathname. If no player is found with the given
-// number, then a "player not found" message is displayed.
-const Player = (props) => {
-    const player = PlayerAPI.get(
-        parseInt(props.match.params.number, 10)
-    )
-    if (!player) {
-        return <div>Sorry, but the player was not found</div>
-    }
-    return (
+// var h1 = React.createElement('h1' ,null ,'Hello world');
+//
+// ReactDOM.render(
+//     h1,
+//     document.getElementById('react')
+// );
+//
+// console.log(document.getElementById('react'));
+if (document.getElementById('react')){
+    ReactDOM.render(
         <div>
-            <h1>{player.name} (#{player.number})</h1>
-            <h2>Position: {player.position}</h2>
-            <Link to='/roster'>Back</Link>
-        </div>
-    )
+            <h1>Hello Alex roza</h1>
+            <h1>Hello pall </h1>
+        </div> ,
+        document.getElementById('react')
+    );
 }
 
-// The Roster component matches one of two different routes
-// depending on the full pathname
-const Roster = () => (
-    <Switch>
-        <Route exact path='/roster' component={FullRoster}/>
-        <Route path='/roster/:number' component={Player}/>
-    </Switch>
-)
+let Links = document.getElementById('Link');
 
-const Schedule = () => (
-    <div>
-        <ul>
-            <li>6/5 @ Evergreens</li>
-            <li>6/8 vs Kickers</li>
-            <li>6/14 @ United</li>
-        </ul>
-    </div>
-)
-
-const Home = () => (
-    <div>
-        <h1>Welcome to the Tornadoes Website!</h1>
-    </div>
-)
-
-// The Main component renders one of the three provided
-// Routes (provided that one matches). Both the /roster
-// and /schedule routes will match any pathname that starts
-// with /roster or /schedule. The / route will only match
-// when the pathname is exactly the string "/"
-const Main = () => (
-    <main>
-        <Switch>
-            <Route exact path='/' component={Home}/>
-            <Route path='/roster' component={Roster}/>
-            <Route path='/schedule' component={Schedule}/>
-        </Switch>
-    </main>
-)
-
-// The Header creates links that can be used to navigate
-// between routes.
-const Header = () => (
-    <header>
-        <nav>
-            <ul>
-                <li><Link to='/'>Home</Link></li>
-                <li><Link to='/roster'>Roster</Link></li>
-                <li><Link to='/schedule'>Schedule</Link></li>
-            </ul>
-        </nav>
-    </header>
-)
-
-const App = () => (
-    <div>
-        <Header />
-        <Main />
-    </div>
-)
-
-// This demo uses a HashRouter instead of BrowserRouter
-// because there is no server to match URLs
-ReactDOM.render((
-    <HashRouter>
-        <App />
-    </HashRouter>
-), onsole.log(Links.children[0].href);
+console.log(Links.children[0].href);
 
 
 // const home = createClass(
